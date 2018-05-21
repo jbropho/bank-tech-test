f2d51742995111de762c4a7a166fddaade21a196
Jordan Brooks <jorbrooks92@gmail.com>
2018-05-21 15:41:49 +0100

    refactor deposit/withdraw methods to interact with accountStatement

--- src/account.js
+++ src/account.js
@@ -3,12 +3,22 @@ var accountStatement = require('./accountStatement.js');
 function bankAccount(statement = accountStatement()) {
   var balance = 0;
 
+  function makeDeposit(amount) {
+    statement.makeDeposit(amount, balance);
+    balance += amount;
+  }
+
+  function makeWithdrawal(amount) {
+    statement.makeWithdrawal(amount, balance);
+    balance -= amount;
+  }
+   
   return {
     deposit: function(amount) {
-      balance += parseFloat(amount);
+      makeDeposit(amount);
     },
     withdraw: function(amount) {
-      balance -= amount;
+      makeWithdrawal(amount);
     },
     getBalance: function(amount) {
       return balance;
